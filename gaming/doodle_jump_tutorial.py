c9e4b0ae28dec419a6edb9028db9b8e5e0b9eeb4
David Scobie <david.scobie.20@ucl.ac.uk>
2026-02-20 16:34:50 +0000

    multiple platforms, no screen scroll yet, 53:28 in video

--- gaming/doodle_jump_tutorial.py
+++ gaming/doodle_jump_tutorial.py
@@ -1,4 +1,4 @@
-#inspired from: https://www.youtube.com/watch?v=qmcgrk5KfHQ
+#inspired from: https://www.youtube.com/watch?v=qmcgrk5KfHQ 
 import pygame
 pygame.init()
 
@@ -19,9 +19,11 @@ timer = pygame.time.Clock()
 #game variables
 player_x = 170
 player_y = 400
-platforms = [[175,480,70,10]] 
+platforms = [[175,480,70,10], [85,370,70,10], [265,370,70,10], [200,260,70,10], [85,150,70,10], [265,150,70,10], [75,40,70,10]] 
 jump = False #is the character jumping?
 y_change = 0
+x_change = 0
+player_speed = 3
 
 #create screen
 screen = pygame.display.set_mode([WIDTH,HEIGHT])
@@ -34,7 +36,7 @@ def check_collisions(rect_list,j):
     global y_change
     #for each platform, is it: (colliding with our player) while the (player is not jumping) and the (player is falling down)?
     for i in range (len(rect_list)): 
-        if rect_list[i].colliderect([player_x, player_y + 60, 90, 10]) and jump == False and y_change > 0:
+        if rect_list[i].colliderect([player_x +30, player_y + 60, 30, 5]) and jump == False and y_change > 0:
             j = True #j is jump
     return j
 
@@ -43,14 +45,21 @@ def update_player(y_pos):
     global jump 
     global y_change
     jump_height = 10
-    gravity = 1 #a greater value of y is lower down in the screen
+    gravity = .4 #a greater value of y is lower down in the screen
     if jump:
-        y_change -= jump_height #a greater value of y is lower down in the screen
+        y_change = -jump_height #a greater value of y is lower down in the screen
         jump = False
     y_pos += y_change 
     y_change += gravity #this is the kinematics quadratic update
     return y_pos
 
+"""
+#handle movement of platforms as game progresses
+def update_platforms(my_list,y_pos,change):
+    if player_y < 250 and y_change < 0:  #if we are (near the top of the screen) and (moving upwards)
+        for i in range (len(my_list)):
+            my_list[i][1] -= change
+"""
 
 running = True
 while running == True:
@@ -66,9 +75,23 @@ while running == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #end the game if instructed
             running = False
+        if event.type == pygame.KEYDOWN: #if a key is pressed
+            if event.key == pygame.K_LEFT: #if the key 'left' is pressed
+                x_change = -player_speed
+            if event.key == pygame.K_RIGHT: #if the key 'right' is pressed
+                x_change = player_speed
+        if event.type == pygame.KEYUP: #if a key is pressed
+            if event.key == pygame.K_LEFT: #if the key 'left' is pressed
+                x_change = 0
+            if event.key == pygame.K_RIGHT: #if the key 'right' is pressed
+                x_change = 0
 
-    player_y = update_player(player_y)
     jump = check_collisions(blocks,jump)
+    player_x += x_change
+    player_y = update_player(player_y)
+    """
+    platforms = update_platforms(platforms,player_y,y_change)
+    """
 
     pygame.display.flip()
 pygame.quit()
